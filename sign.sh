ab3dcbd8b73dab9f2615ebdf294e75dedb2ef4f5
ConnorJC <ccatlett2000@mctherealm.net>
2021-02-02 18:35:58 -0500

    Encrypted secrets
    Fixup renovate

--- sign.sh
+++ sign.sh
@@ -3,8 +3,9 @@
 export SEALED_SECRETS_CONTROLLER_NAMESPACE=sealed-secrets
 export SEALED_SECRETS_CONTROLLER_NAME=sealed-secrets
 
-for f in ./cluster/**/*.secret; do
-  if [ ! -f "${f%.secret}" ]; then
-    kubeseal -o yaml < "$f" > "${f%.secret}"
+for f in ./cluster/**/*.secret.yaml; do
+  if [ ! -f "${f%.secret.yaml}" ]; then
+    echo "$f"
+    kubeseal -o yaml < "$f" > "${f%.secret.yaml}.yaml"
   fi
 done
