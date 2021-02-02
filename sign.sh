34593ebf536f98d6caa423f0ac0054faa651eb25
ConnorJC <ccatlett2000@mctherealm.net>
2021-02-02 18:56:32 -0500

    Fixup signing one last time; Add sentry

--- sign.sh
+++ sign.sh
@@ -4,7 +4,7 @@ export SEALED_SECRETS_CONTROLLER_NAMESPACE=sealed-secrets
 export SEALED_SECRETS_CONTROLLER_NAME=sealed-secrets
 
 for f in ./cluster/**/*.secret.yaml; do
-  if [ ! -f "${f%.secret.yaml}" ]; then
+  if [ ! -f "${f%.secret.yaml}.yaml" ]; then
     echo "$f"
     kubeseal -o yaml < "$f" > "${f%.secret.yaml}.yaml"
   fi
